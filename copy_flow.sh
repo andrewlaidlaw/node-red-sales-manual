00c67da0364d8c710a6e54c0cc7fe6ea9a05a86d
DSpurway <53935437+DSpurway@users.noreply.github.com>
2020-06-30 10:22:25 +0100

    Update copy_flow.sh

--- copy_flow.sh
+++ copy_flow.sh
@@ -1,5 +1,4 @@
 #!/bin/sh
 # Copy flows file to new location
-chmod 750 /usr/src/node-red/sales-manual-reader-flow.json
+chmod 750 /usr/src/node-red/bond-film-flow.json
 HOSTNAME_SHORT=`hostname -s`
-#cp -p /usr/src/node-red/sales-manual-reader-flow.json /usr/src/node-red/.node-red/flows_$HOSTNAME_SHORT.json
