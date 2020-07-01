1c86ad2543a540a4a960ed31d0622472dd9c95c9
DSpurway <53935437+DSpurway@users.noreply.github.com>
2020-07-01 17:37:31 +0100

    Update and rename List Bond Films.py to List_Bond_Films.py

--- List_Bond_Films.py
+++ List_Bond_Films.py
@@ -1,15 +1,6 @@
 #!/usr/bin/env python
 # coding: utf-8
 
-# In[ ]:
-
-
-
-
-
-# In[2]:
-
-
 from urllib.request import urlopen
 from bs4 import BeautifulSoup
 import fileinput
@@ -21,22 +12,3 @@ soup = BeautifulSoup(html, 'html')
 Bond_Film_Quick_List = soup.find("p", string="This is the same list as above, only spoiler-free and much quicker to read:")
 Bond_Film_Date_List=Bond_Film_Quick_List.next_sibling.text
 print(Bond_Film_Date_List)
-
-
-# In[ ]:
-
-
-
-
-
-# In[ ]:
-
-
-
-
-
-# In[ ]:
-
-
-
-
