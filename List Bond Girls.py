ff7c825b12c4f5297ed43ac09b1dff01b898af22
DSpurway <53935437+DSpurway@users.noreply.github.com>
2020-07-01 17:23:01 +0100

    Add files via upload

---
+++ List Bond Girls.py
@@ -0,0 +1,59 @@
+#!/usr/bin/env python
+# coding: utf-8
+
+# In[1]:
+
+
+
+
+
+# In[199]:
+
+
+from urllib.request import urlopen
+from bs4 import BeautifulSoup
+import fileinput
+
+url = "https://en.wikipedia.org/wiki/Bond_girl"
+html = urlopen(url)
+soup = BeautifulSoup(html, 'html')
+# soup.contents
+#print(soup)
+Femme_Fatale_href = soup.find('a',href="/wiki/Femme_fatale")
+Bond_Girls_Table=Femme_Fatale_href.find_next('table')
+Bond_Girls_Text_List=Bond_Girls_Table.text
+#print(Bond_Girls_Text_List)
+
+Actress_Count=Bond_Girls_Text_List.count("(")
+Char=0
+End=0
+Actresses_List = []
+
+for Actress in range(Actress_Count):
+    Start = Bond_Girls_Text_List.find("(", End)
+    Start += 1  # skip the bracket, move to the next character
+    End = Bond_Girls_Text_List.find(")", Start)
+    Result=Bond_Girls_Text_List[Start:End]
+    if Actresses_List.count(Result) == 0:
+        Actresses_List.append(Result)    
+    
+print(Actresses_List)
+
+
+# In[ ]:
+
+
+
+
+
+# In[ ]:
+
+
+
+
+
+# In[ ]:
+
+
+
+
