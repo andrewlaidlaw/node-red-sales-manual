dc050f1e16318a5d7681483741c677e26e7db49e
DSpurway <53935437+DSpurway@users.noreply.github.com>
2020-07-02 17:06:43 +0100

    Update List_Bond_Films.py

--- List_Bond_Films.py
+++ List_Bond_Films.py
@@ -12,3 +12,4 @@ soup = BeautifulSoup(html, 'html')
 Bond_Film_Quick_List = soup.find("p", string="This is the same list as above, only spoiler-free and much quicker to read:")
 Bond_Film_Date_List=Bond_Film_Quick_List.next_sibling.text
 print(Bond_Film_Date_List)
+exit()
