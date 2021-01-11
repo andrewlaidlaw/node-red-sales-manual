652fd93ae54f2e4ec547f5b4c61208fa8fa0aaf1
andrewlaidlaw <36501534+andrewlaidlaw@users.noreply.github.com>
2021-01-11 14:49:30 +0000

    Adding smreader webapp

---
+++ smreader-webapp/app.py
@@ -0,0 +1,47 @@
+from flask import Flask, request
+from urllib.request import urlopen
+from bs4 import BeautifulSoup
+import json
+# import sys
+
+app = Flask(__name__)
+
+@app.route('/')
+def index():
+    content = {}
+
+    if request.args.get('url'):
+        url = request.args.get('url')
+        content['result'] = "Found URL"
+        content['url'] = url
+
+        html = urlopen(url)
+        soup = BeautifulSoup(html, 'html.parser')
+
+        Product_Life_Cycle_Title = soup.find("a", string="Product life cycle dates")
+        Product_Life_Cycle_Table = Product_Life_Cycle_Title.find_next("table")
+        MTM = Product_Life_Cycle_Table.find_next('td')
+        Announce = MTM.find_next('td')
+        Available = Announce.find_next('td')
+        WDFM = Available.find_next('td')
+        EOS = WDFM.find_next('td')
+
+        content['mtm'] = MTM.get_text()
+        content['announce'] = Announce.get_text()
+        content['available'] = Available.get_text()
+        content['wdfm'] = WDFM.get_text()
+        content['eos'] = EOS.get_text()
+
+        content['result'] = "Success"
+
+    else:
+        content ['result'] = "URL Missing"
+
+    return json.dumps(content)
+
+@app.route('/healthz')
+def healthz():
+    return "ok"
+
+if __name__ == '__main__':
+    app.run(debug=True, host='0.0.0.0', port=8080)
